ad05cbb1a1a9d76a96a63fc1d698287e28ae1f58
ShenSeanChen <seanchen9832@gmail.com>
2022-11-29 20:58:36 -0500

    add grid, use 4 cols for picture nft and 6 columns for main section; learnt how to use a flex box vs grid and how to add a horizontal line. Also learnt to use justify-between to space out the components between each other

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,11 +12,19 @@ const Home: NextPage = () => {
 
   // 2. docs.thirdweb.com/react
   // 3. git remote -v --> check if the git repo is the right one. If not --> git remote remove origin
+        // Use the create new branch option instead of adding to existing folder option when creating new repo
         // -> git remote add origin https://github.com/shenseanchen/nftDrop-deploy.git
         // if you get error config bad config option: userkeychain -> open ~/.ssh/config -> https://www.unixtutorial.org/ssh-bad-configuration-option-usekeychain/
         // -> git remote set-url origin https://github.com/<user_name>/<repo_name>.git
             // https://stackoverflow.com/questions/30068298/git-fatal-could-not-read-from-remote-repository-please-make-sure-you-have-th 
   // 3. Create a folder called nft under pages and add in [id].tsx for NFTDROP collections
+  // 4. Responsive design ([id].tsx): 
+        // -> lg:h-96 lg:w-72
+        // -> flex on mobile(default) screen and grid on large screen: flex h-screen flex-col lg:grid lg:grid-cols-10
+        // -> make one div to take up 4 columns in large screen: lg:col-span-4 
+        // Space out components b/w each other -> justify-between
+        // use up as much room as possible in a flex box -> flex flex-1
+        // add horizontal line: <hr />
 
 
   return (
--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,25 +2,44 @@ import React from 'react'
 
 function NFTDropPage() {
   return (
-    <div className='flex h-screen flex-col'>
+    <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
         {/* Left-side Screen */}
-        <div className='bg-gradient-to-br from-cyan-800 to-rose-500'>
-            <div className=''>
-                <img 
-                className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'
-                src="https://links.papareact.com/8sg" alt="" />
-                <div>
-                    <h1>
+        <div className='lg:col-span-4 bg-gradient-to-br from-cyan-800 to-rose-500'>
+            <div className='flex flex-col items-center justify-center py-2 lg:min-h-screen'>
+                <div className='bg-gradient-to-br from-yellow-400 to-purple-600 p-2 rounded-xl'>
+                    <img 
+                    className='w-44 rounded-xl object-cover lg:h-96 lg:w-72'
+                    src="https://links.papareact.com/8sg" alt="" />
+                </div>
+                
+                <div className='space-y-2 text-center p-5'>
+                    <h1 className='text-4xl font-bold text-white'>
                         ShenSeanChen Punk!
                     </h1>
+                    <h2 className='text-xl text-gray-300'>
+                        A collection of ShenSeanChen Dope Apes !
+                    </h2>
                 </div>
             </div>
         </div>
 
 
         {/* Right-side Screen */}
-        <div className='bg-gradient-to-br from-orange-300 to-violet-500'>
-            <h1>sup</h1>
+        <div className='lg:col-span-6 flex flex-1 flex-col p-12 bg-gradient-to-br from-orange-100 to-violet-200'>
+            {/* Header */}
+            <header className='flex items-center justify-between'>
+                <h1 className='w-52 cursor-pointer text-lg font-extralight sm:w-80'>
+                    THE{' '} <span className='font-extrabold underline decoration-pink-600/50'>ShenSeanChen</span> {' '}NFT Marketplace
+                </h1>
+
+                <button className='lg:px-5 lg:py-3 rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold'>Sign In</button>
+            </header>
+
+            <hr className='my-2 border border-rose-300'/>
+
+            {/* Content */}
+
+            {/* Mint Button */}
         </div>
     </div>
   )
