33a984787b734067c7d5eda82ba4ca52553579f7
ShenSeanChen <seanchen9832@gmail.com>
2022-12-06 18:47:39 -0800

    check if I remove thirdweb, would that help me with the bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+// import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <ThirdwebProvider desiredChainId={ChainId.Mainnet}> Eth main network
     // test network -> can use fake eth to test transactions
-    <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
+    // <ThirdwebProvider desiredChainId={ChainId.Rinkeby}>
       <Component {...pageProps} />
-    </ThirdwebProvider>
+    // </ThirdwebProvider>
   )
 }
 
